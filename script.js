b7b1cce32a7101375663e3b64a9c1039e849a337
Devan E <saragrocerynaturals@gmail.com>
2026-07-26 00:37:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
 const products = [
 {
 name:"HDPE Grow Bag 12x12",
-price:"₹249",
+price:249,
 image:"growbag.png"
 },
 {
 name:"HDPE Grow Bag 12x15",
-price:"₹199",
+price:199,
 image:"growbag12x15.png"
 },
 {
 name:"Cocopeat Block 5 KG",
-price:"₹249",
-image:"Cocopeat .png" 
+price:249,
+image:"Cocopeat .png"
 }
 ];
 
 const productList = document.getElementById("product-list");
 
-if (productList) {
+let cart = [];
+
 products.forEach(product => {
 productList.innerHTML += `
 <div class="card">
 <img src="${product.image}" alt="${product.name}">
 <h3>${product.name}</h3>
-<p>${product.price}</p>
-<a class="btn" href="https://wa.me/918428538170?text=I want to buy ${encodeURIComponent(product.name)}">Buy Now</a>
+<p>₹${product.price}</p>
+
+<button class="btn" onclick="addToCart('${product.name}',${product.price})">
+🛒 Add to Cart
+</button>
+
 </div>
 `;
 });
+
+function addToCart(name, price){
+cart.push({name,price});
+
+document.getElementById("cart-count").innerText = cart.length;
+
+alert(name + " Added to Cart");
+}
+
+function checkoutWhatsApp(){
+
+if(cart.length==0){
+alert("Cart is Empty");
+return;
+}
+
+let message="Hello, I want to order:%0A%0A";
+
+let total=0;
+
+cart.forEach((item,index)=>{
+message += `${index+1}. ${item.name} - ₹${item.price}%0A`;
+total += item.price;
+});
+
+message += `%0ATotal : ₹${total}`;
+
+window.open(
+`https://wa.me/918428538170?text=${message}`,
+"_blank"
+);
+
 }
