ecace51f56673e1d698445a1e5a663aa8a8128d0
Devan E <saragrocerynaturals@gmail.com>
2026-07-26 10:18:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-import {
-  addDoc,
-  collection
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+const addDoc = window.addDoc;
+const collection = window.collection;
 const db = window.db;
 const products = [
 {
